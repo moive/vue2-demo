a023ceb49800275a227aa36d35575161e319d1ca
Moises Velasquez <mvelasquez@pictoric.com>
2021-11-30 21:03:57 -0500

    computed property

--- src/scripts/app/components/test.vue
+++ src/scripts/app/components/test.vue
@@ -1,12 +1,22 @@
 <template>
-	<div>{{message}}</div>
+	<div>
+		<div>{{message}}</div>
+		<div>{{fullname}}</div>
+	</div>
 </template>
 
 <script lang="ts">
 	export default {
 		name: 'test',
 		data: ()=>({
-			message:'welcome...!'
-		})
+			message:'welcome...!',
+			name: 'Moises',
+			lastname: 'Velasquez'
+		}),
+		computed:{
+			fullname(){
+				return  `${this.name} ${this.lastname}`;
+			}
+		}
 	}
 </script>
